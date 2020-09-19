ada0f4fcf34c04b7917d612602a35f911547c961
byfed <bbyfed@gmail.com>
2020-09-20 01:25:27 +0200

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://byfed:20200803@cluster0.m8cal.mongodb.net/cafe';
+    urlDB = process.env.mongo_url;
 }
 
 process.env.URLDB = urlDB;
